98715d78af867bb526c3bc9513a90ca25cb81126
tharhtooK <tharhtookyaw.9@gmail.com>
2025-05-06 15:18:55 +0700

    fix: response 200

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #[allow(unused_imports)]
+use std::io::Write;
 use std::net::TcpListener;
+use std::net::TcpStream;
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -8,12 +10,18 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut _stream) => {
+                handle_client(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_client(mut stream: TcpStream){
+    println!("accepted new connection");
+    let resp = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write_all(resp.as_bytes()).unwrap();
 }
