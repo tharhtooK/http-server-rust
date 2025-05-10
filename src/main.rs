e8f3f1d91812e348ee9b2c5130af7a4a551cfa90
tharhtooK <tharhtookyaw.9@gmail.com>
2025-05-10 11:35:06 +0700

    response with body

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-#[allow(unused_imports)]
 use std::io::BufRead;
 use std::io::BufReader;
 use std::io::Write;
 use std::net::TcpListener;
 use std::net::TcpStream;
+#[allow(unused_imports)]
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -25,24 +25,45 @@ fn main() {
 fn handle_client(mut stream: TcpStream){
     println!("accepted new connection");
     let buf_reader = BufReader::new(&stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let lines = &mut buf_reader.lines();
+    let request_line = lines.next().unwrap().unwrap();
+
+    let resp_200 = "HTTP/1.1 200 OK";
+    let resp_404 = "HTTP/1.1 404 Not Found";
 
     let uri = request_line.split_whitespace().nth(1).unwrap();
     let status_line = if uri == "/" {
-        "HTTP/1.1 200 OK"
+        resp_200
     } else if uri.starts_with("/echo") {
-        "HTTP/1.1 200 OK"
+        resp_200
+    } else if uri.starts_with("/user-agent") {
+        resp_200
     } else {
-        "HTTP/1.1 404 Not Found"
+        resp_404
     };
 
-    let request_uri: &str = request_line.split_whitespace().nth(1).unwrap();
-    let uri_params: &str = request_uri.split("/").nth(2).unwrap_or("");
+    let mut user_agent = String::new();
+    for line in lines {
+        let line_ = line.unwrap();
+        let l = &line_;
+        if l.is_empty() {
+            break;
+        }
+        if l.starts_with("User-Agent") {
+            user_agent = l.split_once(": ").unwrap_or(("", "")).1.to_string();
+        }
+    }
 
-    let length = uri_params.len();
+    let request_uri: &str = request_line.split_whitespace().nth(1).unwrap();
+    let body = if uri.starts_with("/echo") {
+        request_uri.split("/").nth(2).unwrap_or("").to_string()
+    } else if uri.starts_with("/user-agent") {
+        user_agent.clone()
+    } else {
+        String::new()
+    };
 
     let response =
-        format!("{status_line}\r\nContent-Type: text/plain\r\nContent-Length: {length}\r\n\r\n{uri_params}");
-
+        format!("{status_line}\r\nContent-Type: text/plain\r\nContent-Length: {len}\r\n\r\n{user_agent}", len=body.len(), user_agent=body.clone());
     stream.write_all(response.as_bytes()).unwrap();
 }
