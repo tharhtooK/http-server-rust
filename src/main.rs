75c96a6f8d98117415159b291749b773384f472a
tharhtooK <tharhtookyaw.9@gmail.com>
2025-05-22 15:57:47 +0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,47 @@
 use std::{
-    collections::HashMap, fs, io::{BufRead, BufReader, Error, Read, Write}, net::{TcpListener, TcpStream}, thread
+    collections::HashMap, fmt::{self, format}, fs, io::{BufRead, BufReader, Error, Read, Write}, net::{TcpListener, TcpStream}, thread
 };
 #[allow(unused_imports)]
+
+enum HttpCode {
+    OK,
+    Created,
+    NotFound,
+}
+
+impl fmt::Display for HttpCode {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", match &self {
+            Self::OK => "HTTP/1.1 200 OK".to_string(),
+            Self::Created => "HTTP/1.1 201 Created".to_string(),
+            Self::NotFound => "HTTP/1.1 404 Not Found".to_string(),
+        })
+    }
+}
+
+enum ContentType {
+    None,
+    Text,
+    Octet
+}
+
+impl fmt::Display for ContentType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", match &self {
+            Self::None => "".to_string(),
+            Self::Text => "text/plain".to_string(),
+            Self::Octet => "application/octet-stream".to_string(),
+        })
+    }
+}
 
 #[derive(Debug)]
 struct Request {
     uri: String,
     http_method: String,
+    encoding: String,
     user_agent: Option<String>,
+    echo_path: String,
     body: String,
     file_name: String,
 }
@@ -15,40 +49,38 @@ struct Request {
 impl Request {
     fn get_http_body(buf_reader: &mut BufReader<&mut TcpStream>, request_line: &String) -> String {
         let mut size = 0;
-        let linesplit = request_line.split("\n")
-                                                .filter_map(|l| 
-                                                    l.split_once(":")
-                                                    .map(|l| (l.0, l.1.trim()))
-                                                    .filter(|l| l.0.starts_with("Content-Length"))
-                                                );
-        // for l in linesplit {
-        //     if l.starts_with("Content-Length") {
-        //         let sizeplit = l.split(":");
-        //         for s in sizeplit {
-        //             if !(s.starts_with("Content-Length")) {
-        //                 size = s.trim().parse::<usize>().unwrap();
-        //             }
-        //         }
-        //     }
-        // }
+        let _: Vec<_> = request_line.split("\n")
+                        .filter_map(|l| 
+                            l.split_once(":")
+                            .filter(|l| l.0.starts_with("Content-Length"))
+                            .map(|l| size = l.1.trim().parse::<usize>().unwrap())
+                        ).collect();
+
         let mut buffer = vec![0; size]; 
         buf_reader.read_exact(&mut buffer).unwrap();
         String::from_utf8(buffer).unwrap()
     }
 
-    fn get_uri(request_line: &String) -> &str {
-        request_line.split_whitespace().nth(1).unwrap()
+    fn get_uri(request_line: &String) -> String {
+        request_line.split_whitespace().nth(1).unwrap().to_string()
     }
 
-    fn get_http_method(request_line: &String) -> &str {
-        request_line.split_whitespace().nth(0).unwrap()
+    fn get_http_method(request_line: &String) -> String {
+        request_line.split_whitespace().nth(0).unwrap().to_string()
+    }
+
+    fn get_echo_path(uri: &String) -> String {
+        uri.split("/").nth(2).unwrap_or("").to_string()
     }
 
     fn get_header_map(request_line: &String) -> HashMap<String, String> {
         let mut request_map: HashMap<String, String> = HashMap::new();
         let res: Vec<_> = request_line.split("\n")
                         .filter_map(|l|                         
-                            Some(l.split_once(":").map(|l| (l.0.to_string(), l.1.trim().to_string())))
+                            Some(
+                                l.split_once(":")
+                                .map(|l| (l.0.to_string(), l.1.trim().to_string()))
+                            )
                             .take_if(|l| !l.is_none())
                         )
                         .collect();
@@ -73,32 +105,50 @@ impl Request {
                 break;
             }
         }
-        let uri = Self::get_uri(&request_line).to_string();
+        let uri = Self::get_uri(&request_line);
         let file_name = Self::get_file_name(&uri);
-        let http_method = Self::get_http_method(&request_line).to_string();
-        let body = Self::get_http_body(&mut buf_reader, &request_line).to_string();
+        let http_method = Self::get_http_method(&request_line);
+        let body = Self::get_http_body(&mut buf_reader, &request_line);
         let headers_map = Self::get_header_map(&request_line);
+        let encoding = headers_map.get("Accept-Encoding").unwrap_or(&"invalid-encoding".to_string()).to_string();
         let user_agent = headers_map.get("User-Agent").map(|v| v.to_string());
+        let echo_path = Self::get_echo_path(&uri);
 
         Self {
-            uri: uri,
-            file_name: file_name,
-            http_method: http_method,
-            user_agent: user_agent,
-            body: body,
+            uri,
+            http_method,
+            encoding,
+            user_agent,
+            echo_path,
+            body,
+            file_name,
         }
     }
 }
 
 struct Response {
-    status_line: String,
-    content_type: String,
+    status: HttpCode,
+    content: ContentType,
+    encoding: String,
     body: String,
 }
 
+impl fmt::Display for Response {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let encoding_content = match self.encoding.as_str() {
+            "gzip" => format!("Content-Encoding: {}\r\n", self.encoding),
+            _ => "".to_string()
+        };
+        write!(f, "{status}\r\nContent-Type: {content}\r\n{encoding_content}Content-Length: {len}\r\n\r\n{body}",
+            status=self.status,
+            len=self.body.len(),
+            content=self.content,
+            body=self.body.clone()
+        )
+    }
+}
+
 fn main() {
-    println!("Logs from your program will appear here!");
-    
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
@@ -117,17 +167,83 @@ fn main() {
 
 fn read_file(file_name: &str) -> Result<String, Error> {
     let file_path = format!("/tmp/data/codecrafters.io/http-server-tester/{file_name}");
-
     fs::read_to_string(file_path)
 }
 
-fn create_file(file_name: &str, content: String) -> Result<(), Error> {
+fn create_file(file_name: &str, content: &String) -> Result<(), Error> {
     let file_path = format!("/tmp/data/codecrafters.io/http-server-tester/{file_name}");
     let file = fs::File::create(file_path);
     file.expect("File not found!").write_all(content.as_bytes())
 }
 
-fn handle_client(mut stream: TcpStream){
+fn handle_files_routes(req: &Request) -> Response {
+    match req.http_method.as_str() {
+        "POST" => match create_file(&req.file_name, &req.body) {
+            Ok(_) => Response {
+                status: HttpCode::Created, 
+                content: ContentType::None, 
+                encoding: req.encoding.to_string(),
+                body:"".to_string()
+            },
+            Err(_) => Response {
+                status: HttpCode::NotFound, 
+                content: ContentType::None, 
+                encoding: req.encoding.to_string(),
+                body:"".to_string()
+            }
+        },
+        _ => match read_file(&req.file_name) {
+            Ok(content) => Response {
+                status: HttpCode::OK, 
+                content: ContentType::Octet, 
+                encoding: req.encoding.to_string(),
+                body: content
+            },
+            Err(_) => Response {
+                status: HttpCode::NotFound, 
+                content: ContentType::Text, 
+                encoding: req.encoding.to_string(),
+                body: "".to_string()
+            }
+        }
+    }
+}
+
+fn handle_routes(req: &Request) -> Response {
+    if req.uri == "/" {
+        Response {
+            status: HttpCode::OK, 
+            content: ContentType::Text, 
+            encoding: req.encoding.to_string(),
+            body: "".to_string()
+        }
+    } else if req.uri.starts_with("/echo") {
+        Response {
+            status: HttpCode::OK, 
+            content: ContentType::Text, 
+            encoding: req.encoding.to_string(),
+            body: req.echo_path.to_string()
+        }
+    } else if req.uri.starts_with("/user-agent") {
+        Response {
+            status: HttpCode::OK, 
+            content: ContentType::Text, 
+            encoding: req.encoding.to_string(),
+            body: req.user_agent.clone().unwrap()
+        }
+    } else if req.uri.starts_with("/files") {
+        handle_files_routes(req)
+    } else {
+        Response {
+            status: HttpCode::NotFound, 
+            content: ContentType::Text, 
+            encoding: req.encoding.to_string(),
+            body: "".to_string()
+        }
+    }
+}
+
+fn handle_client(stream: TcpStream){
     println!("accepted new connection");
     let mut stream = stream;
     let req: Request;
@@ -135,52 +251,8 @@ fn handle_client(mut stream: TcpStream){
         let stream2 = &mut stream;
         req = Request::new(stream2);
     }
-
-    let resp_200 = "HTTP/1.1 200 OK";
-    let resp_201 = "HTTP/1.1 201 Created";
-    let resp_404 = "HTTP/1.1 404 Not Found";
-    let content_type_text = "text/plain";
-    let content_type_octet = "application/octet-stream";
-
-    let (status_line, content_type, body) = if req.uri == "/" {
-        (resp_200, content_type_text, "".to_string())
-    } else if req.uri.starts_with("/echo") {
-        let echo = req.uri.split("/").nth(2).unwrap_or("");
-        (resp_200, content_type_text, echo.to_string())
-    } else if req.uri.starts_with("/user-agent") {
-        (resp_200, content_type_text, req.user_agent.unwrap())
-    } else if req.uri.starts_with("/files") {
-        match req.http_method.as_str() {
-            "POST" => {
-                match create_file(&req.file_name, req.body) {
-                    Ok(_) => (resp_201, "", "".to_string()),
-                    Err(_) => (resp_404, "", "".to_string())
-                }
-            },
-            _ => {
-                match read_file(&req.file_name) {
-                    Ok(content) => {
-                        (resp_200, content_type_octet, content)
-                    },
-                    Err(_) => {
-                        (resp_404, content_type_text, "".to_string())
-                    }
-                }
-            }
-        }
-    } else {
-        (resp_404, content_type_text, "".to_string())
-    };
-
-    println!("status: {status_line}, content_type: {content_type}, body: {body}");
+    println!("{req:?}");
+    let response = handle_routes(&req);
     
-    let response = format!(
-        "{status_line}\r\nContent-Type: {content_type}\r\nContent-Length: {len}\r\n\r\n{body}",
-        len=body.len(),
-        content_type=content_type,
-        body=body.clone()
-    );
-    
-    stream.write_all(response.as_bytes()).unwrap();
-    
+    stream.write_all(format!("{response}").as_bytes()).unwrap();
 }
